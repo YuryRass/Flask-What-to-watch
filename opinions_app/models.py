e920341d99c3d5f1bc4378a4954f2afb640d5f39
Yurii <yurchik1997yurchik@gmail.com>
2024-01-02 14:25:42 +0300

    added two methods

--- opinions_app/models.py
+++ opinions_app/models.py
@@ -13,9 +13,22 @@ def now_utc() -> datetime:
 
 @dataclass
 class Opinion(db.Model):
+    """Отзыв на фильм"""
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(128))
     text: Mapped[str] = mapped_column(Text, unique=True)
     source: Mapped[str | None] = mapped_column(String(256))
     timestamp: Mapped[datetime] = mapped_column(index=True, default=now_utc)
     added_by: Mapped[str] = mapped_column(String(64))
+
+    def update(self, data: dict) -> None:
+        """Изменение атрибутов модели"""
+        opinion_attrs = [clmn.key for clmn in self.__table__.columns]
+        for attr in opinion_attrs:
+            setattr(self, attr, data.get(attr, getattr(self, attr)))
+
+    def filling(self, data: dict) -> None:
+        """Заполнение новыми данными объекта модели"""
+        for field in ["title", "text", "source", "added_by"]:
+            if field in data:
+                setattr(self, field, data[field])
